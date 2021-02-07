5bdd25b24b4ef0a756107cdc13a7f0f4ae63a3ff
Sergey Pomortsev <zaooooza@gmail.com>
2021-02-07 19:56:21 +0300

    created web and connect with back

--- app.js
+++ app.js
@@ -2,9 +2,11 @@ const express = require('express');
 const config = require('config');
 const mongoose = require('mongoose');
 const app = express();
-const PORT = config.get('port') || 5000;
 
+app.use(express.json({extended: true}))
 app.use('/api/auth', require('./routes/auth.routes'));
+
+const PORT = config.get('port') || 5000;
 
 async function start () {
   try {
