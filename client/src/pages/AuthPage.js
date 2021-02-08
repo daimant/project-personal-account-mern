326e25032a5898e2e3df969f04d2cf0299bd2fad
Sergey Pomortsev <zaooooza@gmail.com>
2021-02-08 17:13:09 +0300

    added auth logic in front

--- client/src/pages/AuthPage.js
+++ client/src/pages/AuthPage.js
@@ -1,8 +1,10 @@
-import React, {useState,useEffect} from 'react';
+import React, {useState,useEffect,useContext} from 'react';
 import {useHttp} from "../hooks/http.hook";
 import {useMessage} from "../hooks/message.hook";
+import {AuthContext} from "../context/AuthContext";
 
 export const AuthPage = () => {
+  const auth = useContext(AuthContext);
   const message = useMessage();
   const {loading, error, request, clearError} = useHttp();
   const [form, setForm] = useState({
@@ -13,6 +15,10 @@ export const AuthPage = () => {
     message(error);
     clearError();
   }, [error, message, clearError]);
+
+  useEffect(() => {
+    window.M.updateTextFields();
+  }, []);
 
   const changeHandler = event => {
     setForm({ ...form, [event.target.name]: event.target.value})
@@ -25,10 +31,17 @@ export const AuthPage = () => {
     } catch (e) {}
   };
 
+  const loginHandler = async () => {
+    try {
+      const data = await request('/api/auth/login', 'POST', {...form});
+      auth.login(data.token, data.userId);
+    } catch (e) {}
+  };
+
   return (
     <div className='row'>
       <div className="col s6 offset-s3">
-        <h1>personal acc</h1>
+        <h1>Сократи ссылку</h1>
         <div className="card blue darken-1">
           <div className="card-content white-text">
             <span className="card-title">Авторизация</span>
@@ -65,6 +78,7 @@ export const AuthPage = () => {
               className='btn yellow darken-4'
               style={{marginRight: 10}}
               disabled={loading}
+              onClick={loginHandler}
             >Войти</button>
             <button
               className='btn grey lighten-1 black-text'
